013f1251fba7a2d769443ecf8c0a50e4321bf659
mohammadmaseeh <m.maseeh@hotmail.com>
2021-01-01 07:54:09 +0300

    Update services.blade.php

--- resources/views/trading/services.blade.php
+++ resources/views/trading/services.blade.php
@@ -1,5 +1,12 @@
 @extends('trading.layout')
 @section('content')
+<style>
+    @media all and (min-width: 767px){
+        .card-img-top{
+            max-width: 300px;
+        }
+    }
+</style>
 <!-- Page title-->
 <section class="position-relative bg-dark pt-7 pb-5 pb-md-7 bg-size-cover bg-fixed" style="background-image: url('{{url('images/trading/cubes-bg.jpg')}}');">
     <div class="cs-shape cs-shape-bottom cs-shape-curve bg-body">
@@ -14,234 +21,65 @@
 <section class="container pt-5 pt-md-6 pt-lg-7">
     <h2 class="text-center pt-3 pt-md-2 pb-4">Our Services/Categories</h2>
     <p class="text-center">We are importer & exporter of many products/categories as following:<br><br></p>
-    <div class="cs-masonry-filterable mb-3">
-        <div class="cs-masonry-grid" data-columns="4">
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/metal.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Metal</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Metal</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top" style="background-image: url('{{url('images/trading/categories/metal.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Metal Import/Export</h2>
+                    <p>Mineral is a naturally occurring, solid, inorganic element or compound having a uniform composition and a regularly repeating internal structure. We offer multiple types of Minerals and Ore in bulk quantity.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/polymer.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Polymers</h3>
-                                        <p class="font-size-sm text-blue">Polymers (Plastic) - Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Polymers</h3>
-                                        <p class="font-size-sm text-blue">Polymers (Plastic) - Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">>Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top order-sm-2" style="background-image: url('{{url('images/trading/categories/polymer.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Polymer Import/Export</h2>
+                    <p>We deal with polymer products. Visit us to get your chance.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/health.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Health Supplements</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Health Supplements</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top" style="background-image: url('{{url('images/trading/categories/health.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Health Supplements Import/Export</h2>
+                    <p>Health suppliments are tranding products for modern era of fitness. We deal with best entities all around the world.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/veg.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Fruits & Vegetables</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Fruits & Vegetables</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top order-sm-2" style="background-image: url('{{url('images/trading/categories/veg.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Fruits & Vegetables Import/Export</h2>
+                    <p>Fruits & Vegetables are necessary daily usage products. We export many products to many countries & import some fruits as well.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/agro-commodities.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Agro Commodities</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Agro Commodities</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top" style="background-image: url('{{url('images/trading/categories/surgical.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Surgical Equipment Import/Export</h2>
+                    <p>India is making cheap & best qualtiy surgical products to fulfill the needs of worlds and we deal in it.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/surgical.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Surgical Equipment</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Surgical Equipment</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top order-sm-2" style="background-image: url('{{url('images/trading/categories/agro-commodities.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Agro Commodities Import/Export</h2>
+                    <p>he major agro products can be broadly grouped into foods, fibers, fuels, and raw materials. We cater for ALL types of Agriculture Products in any quantity required.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/grow.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Grow with Us</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Grow with us</h3>
-                                        <p class="font-size-sm text-blue">We will enhance your Idea & Products for Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top" style="background-image: url('{{url('images/trading/categories/other.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Other Products</h2>
+                    <p>Our product team always search & achive new opportunities to deal with different kind of products. You can contact us regarding your idea.</p>
                 </div>
             </div>
-            <div class="cs-grid-item" data-groups="[&quot;3d&quot;]">
-                <div class="card card-flip">
-                    <div class="card-flip-inner">
-                        <div class="card-flip-front">
-                            <img class="card-img" src="{{url('images/trading/categories/other.jpg')}}" alt="Portfolio thumb"/>
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Other Products</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="card-flip-back" href="{{url('international-trading/services')}}">
-                            <div class="card-body">
-                                <div class="card-body-inner">
-                                    <div class="content-inner">
-                                        <h3 class="h5 card-title mb-2">Other Products</h3>
-                                        <p class="font-size-sm text-blue">Import/Export</p>
-                                        <span class="btn btn-primary btn-sm">Read More..</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <div class="card card-horizontal card-hover mb-grid-gutter">
+                <div class="card-img-top order-sm-2" style="background-image: url('{{url('images/trading/categories/grow.jpg')}}');"></div>
+                <div class="card-body">
+                    <h2>Grow with us</h2>
+                    <p>We always welcome startups & new ideas to work all over the world and grow with us.</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
 @endsection
